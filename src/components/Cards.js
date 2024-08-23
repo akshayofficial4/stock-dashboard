7fb4e02b14d35ff398ecd64b75e14b7b26bfe2d6
Akshay Sunil <akshayofficial43@gmail.com>
2024-08-24 00:54:29 +0530

    makes responsive

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
-import ThemeContext from "../context/ThemeContext";
+import ThemeContext from "../Context/ThemeContext";
 
 const Cards = ({ children }) => {
   const { darkMode } = useContext(ThemeContext);
+
   return (
     <div
-      className={`w-full h-full rounded-md relative p-8 border-2 ${
+      className={`w-full h-full  relative p-8 border-2 mt-5  ${
         darkMode ? "bg-gray-900 border-gray-800" : "bg-white border-neutral-200"
       }`}
     >
