529af921ad95f8d041b43919e4fc0b4f45eed7d8
Akshay Sunil <akshayofficial43@gmail.com>
2024-08-24 01:19:02 +0530

    makes responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,14 +5,13 @@ import ThemeIcon from "./ThemeIcon";
 const Header = ({ name }) => {
   return (
     <>
-    <div className="flex flex-row lg:pt-8 md:pt-11 ">
-      <div className="xl:px-32">
-        <h1 className="2xl:text-5xl xl:text-5xl lg:text-5xl md:text-4xl sm:text-3xl text-2xl pt-3  "> {name} </h1>
-        <Search />
-        
+    <div className='flex flex-row lg:pt-8 md:pt-11 '>
+      <div className='xl:px-32 md:px-40'>
+        <h1 className='2xl:text-4xl xl:text-3xl lg:text-2xl md:text-2xl sm:text-2xl text-2xl pt-6 mt-4 xl:pt-1 '> {name} </h1>
+        <Search/>
       </div>
-      {/* Theme Icon */}
-      <ThemeIcon  />
+      {/*ThemeIcon*/}
+      <ThemeIcon/>
     </div>
     </>
   );
